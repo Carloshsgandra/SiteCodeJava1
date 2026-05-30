734d9e335223e1ac3e00957157032112b2431a05
Carlos Henrique S.B. Gandra <carloshsgandra@gmail.com>
2026-05-30 14:30:23 -0300

    fix: corrige import EncodedResource (core.io.support, não jdbc)

--- src/main/java/com/javaduolingo/service/DatabaseSeeder.java
+++ src/main/java/com/javaduolingo/service/DatabaseSeeder.java
@@ -7,8 +7,8 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.EncodedResource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
-import org.springframework.jdbc.datasource.init.EncodedResource;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
